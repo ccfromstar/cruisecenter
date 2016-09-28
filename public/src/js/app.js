8609552d27395007a8e0ab311ee8ab90b9fffedb
ccfromstar <114413511@qq.com>
2016-09-28 15:33:04 +0800

    update

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -36,7 +36,46 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 					templateUrl: 'tpls/footer.html'
 				},
 				'main@index': {
-					templateUrl: 'tpls/homepage.html'
+					templateUrl: 'tpls/homepage.html',
+					controller: function($scope) {
+						$.ajax({
+							type: "get",
+							url: "http://weather.huiyoulun.com/BsReal",
+							timeout: 3000,
+							success: function(data) {
+								if(!data) {
+									//alert('服务器连接失败！');
+									return false;
+								}
+								$('#Temperature').html('当前温度：' +data.Temperature + '<span>℃</span>');
+								$('#CollectDate').html('更新时间：' +data.CollectDate);
+								$('#Rain_sum_60').html('降水：' + data.Rain_sum_60);
+								$('#RelativeHumidity').html('相对湿度：' + data.RelativeHumidity + ' %');
+								$('#WindSpeed').html('风速：' + data.WindSpeed + ' m/s');
+								$('#Visibility').html('能见度：' + data.Visibility + ' 米');
+								$('#AirPressure').html('气压：' + data.AirPressure);
+
+								$.ajax({
+									type: "get",
+									url: hosts + "/BsWarn",
+									success: function(data) {
+										var _list = '';
+										for(var i in data) {
+											_list += '<h3>' + data[i].WarningLevel + data[i].WarningType + '预警</h3>';
+										}
+										$('#warn').html(_list);
+										$modal.modal('close');
+									}
+								});
+							},
+							complete: function(XMLHttpRequest, status) { //请求完成后最终执行参数
+								if(status == 'timeout') { //超时,status还有success,error等值的情况
+									//ajaxTimeoutTest.abort();　　　　　
+									alert('气象局服务器连接失败！请稍后再访问！');　　　
+								}　　
+							}
+						});
+					}
 				}
 			}
 		})
@@ -326,7 +365,7 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 
 					}
 
-					for(i = 1; i < days+1; i++) {
+					for(i = 1; i < days + 1; i++) {
 						var d = (((i) + "").length == 1) ? "0" + (i) : (i);
 						var time = yyyy + "-" + mm + "-" + d;
 						var time_today = yyyy + "-" + mm + "-" + day;
