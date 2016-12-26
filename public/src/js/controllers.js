c96a2c2bab7fa6035ba38b3725381e4cb4e4d4a9
ccfromstar <114413511@qq.com>
2016-12-26 14:50:12 +0800

    update

--- public/src/js/controllers.js
+++ public/src/js/controllers.js
@@ -603,7 +603,7 @@ headerApp.controller('footerController', function($scope) {
 
 headerApp.controller('homeController', function($scope, $http, $sce) {
 	setBD(1);
-	setFW(2);
+	setFW(1);
 	/*计算行程周历*/
 	var d = new Date;
 	var w = d.getDay();
