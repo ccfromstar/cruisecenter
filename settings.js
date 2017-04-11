675629c841c2034609379fe8aa708717b90e9dbb
ccfromstar <114413511@qq.com>
2017-04-11 15:06:36 +0800

    update

--- settings.js
+++ settings.js
@@ -1,10 +1,3 @@
 module.exports = {
-    host: 'rm-uf6100u5egk87us1eo.mysql.rds.aliyuncs.com',
-    user: 'wskroot',
-    password: 'Ws123456',
-    database:'cruisesh',
-    port: 3306,
-    AppID:'wx630bfbf8158108ec',
-    AppSecret:'5bd836ff035b8eee93609ed92c4db8ff',
-    hosts:'http://www.cruisesh.com'
+    
 };
